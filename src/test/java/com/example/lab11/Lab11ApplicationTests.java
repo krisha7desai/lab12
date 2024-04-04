19d68447aec802326f41613be17a09396b9f8685
krisha7desai <krishad2001@gmail.com>
2024-04-03 20:11:46 -0400

    test added

--- src/test/java/com/example/lab11/Lab11ApplicationTests.java
+++ src/test/java/com/example/lab11/Lab11ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,7 +12,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GreetingControllerTest {
 
-    int port=8090;
+    @LocalServerPort
+    int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
